5b8c61ff82a93f0267abae4ca3ebe739e16a11f5
กีตาร์เอาเอง <guitar@odds.team>
2022-04-11 04:05:04 +0700

    Update AccountService.dart

--- lib/services/AccountService.dart
+++ lib/services/AccountService.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 import '../database/client.dart';
@@ -16,4 +15,32 @@ Future<AccountModel> Login(String email, String password) async {
     },
   );
   return AccountModel.fromJson(jsonDecode(response.body));
+}
+
+Future<dynamic> Register(
+    String email, String password, String name, String imageURL) async {
+  try {
+    final String Url = Host + "/api/account";
+    final response = await http.post(
+      Uri.parse(Url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        "email": email,
+        "password": password,
+        "name": name,
+        "image": imageURL
+      }),
+    );
+
+    if (response.statusCode == 400) {
+      var err = json.decode(json.encode(response.body));
+      return err;
+    }
+    String data = json.decode(json.encode(response.body));
+    return data;
+  } catch (e) {
+    print(e);
+  }
 }
