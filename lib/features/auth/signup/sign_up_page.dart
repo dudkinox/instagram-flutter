df7d11718d97e5927d26da352e7de78f5873c7d6
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-04-11 06:38:59 +0700

    🥱 validate form register

--- lib/features/auth/signup/sign_up_page.dart
+++ lib/features/auth/signup/sign_up_page.dart
@@ -2,151 +2,188 @@
 
 import 'dart:io';
 
-import 'package:async/async.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
 
 class SignUpPage extends StatefulWidget {
-  SignUpPage({Key? key}) : super(key: key);
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
   State<SignUpPage> createState() => _SignUpPageState();
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  static const TAG = 'SignUpPage';
-  ResultFuture<bool>? singingFuture;
   late File file;
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool isPasswordValid(String password) => password.length >= 6;
 
   @override
   Widget build(BuildContext context) {
     final inputBorder =
         OutlineInputBorder(borderSide: Divider.createBorderSide(context));
-    //register form
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       body: IgnorePointer(
         child: SafeArea(
           child: SingleChildScrollView(
             child: Container(
               padding: EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 64,
-                  ),
-                  SvgPicture.asset(
-                    'assets/icons/ic_instagram.svg',
-                    color: Theme.of(context).colorScheme.onBackground,
-                    height: 32,
-                  ),
-                  SizedBox(
-                    height: 64,
-                  ),
-                  IconButton(
-                    onPressed: () async {
-                      var image = await ImagePicker().getImage(
-                        source: ImageSource.gallery,
-                      );
-                    },
-                    icon: Image.asset('assets/images/profile-img.png'),
-                    iconSize: 150,
-                    padding: EdgeInsets.zero,
-                  ),
-                  Text(
-                    'Upload a profile picture',
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'full name',
-                      border: inputBorder,
-                      focusedBorder: inputBorder,
-                      enabledBorder: inputBorder,
-                      filled: true,
-                      contentPadding: EdgeInsets.all(8),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 64,
                     ),
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'email',
-                      border: inputBorder,
-                      focusedBorder: inputBorder,
-                      enabledBorder: inputBorder,
-                      filled: true,
-                      contentPadding: EdgeInsets.all(8),
+                    SvgPicture.asset(
+                      'assets/icons/ic_instagram.svg',
+                      color: Theme.of(context).colorScheme.onBackground,
+                      height: 32,
                     ),
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: 'password',
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        var image = await ImagePicker().getImage(
+                          source: ImageSource.gallery,
+                        );
+                      },
+                      icon: Image.asset('assets/images/profile-img.png'),
+                      iconSize: 150,
+                      padding: EdgeInsets.zero,
+                    ),
+                    Text(
+                      'Upload a profile picture',
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        hintText: 'Full Name',
                         border: inputBorder,
                         focusedBorder: inputBorder,
                         enabledBorder: inputBorder,
                         filled: true,
-                        contentPadding: EdgeInsets.all(8)),
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'confirm password',
-                      border: inputBorder,
-                      focusedBorder: inputBorder,
-                      enabledBorder: inputBorder,
-                      filled: true,
-                      contentPadding: EdgeInsets.all(8),
+                        contentPadding: EdgeInsets.all(8),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  InkWell(
-                    child: Container(
-                      child:
-                          (singingFuture == null || singingFuture!.isComplete)
-                              ? Text(
-                                  'Sign Up',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      ?.copyWith(color: Colors.white),
-                                )
-                              : CupertinoActivityIndicator(),
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.symmetric(vertical: 12),
-                      decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(4)),
-                          ),
-                          gradient: LinearGradient(colors: [
-                            Color(0xFF405de6),
-                            Color(0xFF833ab4),
-                            Color(0xFFe1306c),
-                          ])),
+                    const SizedBox(
+                      height: 24,
                     ),
-                    onTap: () {},
-                  ),
-                ],
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        border: inputBorder,
+                        focusedBorder: inputBorder,
+                        enabledBorder: inputBorder,
+                        filled: true,
+                        contentPadding: EdgeInsets.all(8),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: inputBorder,
+                          focusedBorder: inputBorder,
+                          enabledBorder: inputBorder,
+                          filled: true,
+                          contentPadding: EdgeInsets.all(8)),
+                      obscureText: true,
+                      validator: (password) {
+                        if (isPasswordValid(password!)) {
+                          return null;
+                        } else {
+                          return 'Password must be at least 6 characters';
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Confirm Password',
+                        border: inputBorder,
+                        focusedBorder: inputBorder,
+                        enabledBorder: inputBorder,
+                        filled: true,
+                        contentPadding: EdgeInsets.all(8),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please confirm your password';
+                        } else if (value != _passwordController.text) {
+                          return 'Password do not match';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    InkWell(
+                      child: Container(
+                        child: Text(
+                          'Sign Up',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              ?.copyWith(color: Colors.white),
+                        ),
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        decoration: const ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                            ),
+                            gradient: LinearGradient(colors: [
+                              Color(0xFF405de6),
+                              Color(0xFF833ab4),
+                              Color(0xFFe1306c),
+                            ])),
+                      ),
+                      onTap: () {
+                        if (_formKey.currentState!.validate()) {
+                          print("validate");
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
-        ignoring: singingFuture != null && !singingFuture!.isComplete,
       ),
     );
   }
